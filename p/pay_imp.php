4dd3b551f422713db0e099caa6dc8416511eb47c
t-bizz <tharmendheran@bizappln.com>
2021-11-23 22:35:53 +0530

    update: added test script

--- p/pay_imp.php
+++ p/pay_imp.php
@@ -91,6 +91,27 @@ if ($quotenumber != "") {
     $Allpaymentlinkurl = $creatorbaseurl . "report/All_Payment_Links?Quoteno=" . urlencode($quotenumber) . "&raw=true;";
     $json = getcreatordata($Allpaymentlinkurl);
     echo "<br>".$Allpaymentlinkurl.json_encode($json);
+    $zoho_auth = json_decode(generatetoken(), true);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $Allpaymentlinkurl,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_POSTFIELDS => $body,
+      CURLOPT_HTTPHEADER => $zoho_auth['creator'],
+    ));
+  
+    $json = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+     
+    echo json_encode($json);
+    // $json = curl($creatorurl, "GET", array(), );
     if ($json['code'] == 3000) {
       $finalquote = $json['data'][0];
       $storename = $finalquote['Stores']['display_value'];
