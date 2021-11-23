05f6f946d52332656eb7f311de1a79dfd916ce0c
t-bizz <tharmendheran@bizappln.com>
2021-11-23 22:36:47 +0530

    change : fix var names

--- p/pay_imp.php
+++ p/pay_imp.php
@@ -101,8 +101,7 @@ if ($quotenumber != "") {
       CURLOPT_TIMEOUT => 0,
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => $method,
-      CURLOPT_POSTFIELDS => $body,
+      CURLOPT_CUSTOMREQUEST => 'GET', 
       CURLOPT_HTTPHEADER => $zoho_auth['creator'],
     ));
   
