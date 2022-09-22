9ab5a33834cb81f6a018b2b1fc122505ad839c30
t-bizz <tharmendheran@bizappln.com>
2022-09-22 15:52:09 +0530

    update: endpoint update

--- p/AvalaraCORSWorkaround.php
+++ p/AvalaraCORSWorkaround.php
@@ -6,12 +6,28 @@ $shipstate = $_GET["shipstate"];
 
 // echo "ZipCode: ".$zipcode."<br> Shipcity: ".$Shipcity."<br> Shipstate: ".$shipstate."<br>";
 
-$url = 'https://1.door-pay.com/api/avalatax/byzip/'.$zipcode.'/'.rawurlencode($Shipcity).'::'.$shipstate;
+$url = 'https://1.door-pay.com/api/avalatax/byzip/' . $zipcode . '/' . rawurlencode($Shipcity) . '::' . $shipstate;
 
-// echo $url."<br>";
+echo $url . "<br>";
+
+// $curl = curl_init();
+// curl_setopt_array($curl, array(
+//   CURLOPT_URL => $url,
+//   CURLOPT_RETURNTRANSFER => true,
+//   CURLOPT_ENCODING => '',
+//   CURLOPT_MAXREDIRS => 10,
+//   CURLOPT_TIMEOUT => 0,
+//   CURLOPT_FOLLOWLOCATION => true,
+//   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//   CURLOPT_CUSTOMREQUEST => 'GET',
+// ));
+
+// $response = curl_exec($curl);
+
+// curl_close($curl);
+// echo $response;
 
 $curl = curl_init();
-
 curl_setopt_array($curl, array(
   CURLOPT_URL => $url,
   CURLOPT_RETURNTRANSFER => true,
@@ -22,8 +38,6 @@ curl_setopt_array($curl, array(
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'GET',
 ));
-
 $response = curl_exec($curl);
-
 curl_close($curl);
 echo $response;
